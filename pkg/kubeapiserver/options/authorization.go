8b81cd9062ae15281af0feb398d83985934ca5b5
Nabarun Pal <pal.nabarun95@gmail.com>
2023-08-24 15:57:29 +0530

    add --authorization-config flag and guard reading it using the featureflag
    Signed-off-by: Nabarun Pal <pal.nabarun95@gmail.com>

--- pkg/kubeapiserver/options/authorization.go
+++ pkg/kubeapiserver/options/authorization.go
@@ -21,12 +21,16 @@ import (
 	"strings"
 	"time"
 
+	genericfeatures "k8s.io/apiserver/pkg/features"
+	utilfeature "k8s.io/apiserver/pkg/util/feature"
+
 	"github.com/spf13/pflag"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apimachinery/pkg/util/wait"
 	authzconfig "k8s.io/apiserver/pkg/authorization/config"
+	authzconfigloader "k8s.io/apiserver/pkg/authorization/config/load"
 	authzconfigvalidation "k8s.io/apiserver/pkg/authorization/config/validation"
 	genericoptions "k8s.io/apiserver/pkg/server/options"
 	versionedinformers "k8s.io/client-go/informers"
@@ -46,6 +50,8 @@ type BuiltInAuthorizationOptions struct {
 	// This allows us to configure the sleep time at each iteration and the maximum number of retries allowed
 	// before we fail the webhook call in order to limit the fan out that ensues when the system is degraded.
 	WebhookRetryBackoff *wait.Backoff
+
+	AuthorizationConfigurationFile string
 }
 
 // NewBuiltInAuthorizationOptions create a BuiltInAuthorizationOptions with default value
@@ -66,12 +72,35 @@ func (o *BuiltInAuthorizationOptions) Validate() []error {
 	}
 	var allErrors []error
 
-	authzConfiguration := o.buildAuthorizationConfiguration()
-	if errors := authzconfigvalidation.ValidateAuthorizationConfiguration(nil, authzConfiguration,
-		sets.NewString(authzmodes.AuthorizationModeChoices...),
-		sets.NewString(authzmodes.RepeatableAuthorizerTypes...),
-	); len(errors) != 0 {
-		allErrors = append(allErrors, errors.ToAggregate().Errors()...)
+	if !utilfeature.DefaultFeatureGate.Enabled(genericfeatures.StructuredAuthorizationConfig) && o.AuthorizationConfigurationFile != "" {
+		allErrors = append(allErrors, fmt.Errorf("StructuredAuthorizationConfig is disabled but --authorization-config is set"))
+	}
+
+	if utilfeature.DefaultFeatureGate.Enabled(genericfeatures.StructuredAuthorizationConfig) {
+		if o.AuthorizationConfigurationFile == "" {
+			allErrors = append(allErrors, fmt.Errorf("StructuredAuthorizationConfig is enabled but --authorization-config is not set"))
+		} else {
+			config, err := authzconfigloader.LoadFromFile(o.AuthorizationConfigurationFile)
+			if err != nil {
+				allErrors = append(allErrors, fmt.Errorf("failed to load config from file %s: %s", o.AuthorizationConfigurationFile, err))
+			}
+
+			if errors := authzconfigvalidation.ValidateAuthorizationConfiguration(nil, config,
+				sets.NewString(authzmodes.AuthorizationModeChoices...),
+				sets.NewString(authzmodes.RepeatableAuthorizerTypes...),
+			); len(errors) != 0 {
+				allErrors = append(allErrors, errors.ToAggregate().Errors()...)
+			}
+		}
+	} else {
+		authzConfiguration := o.buildAuthorizationConfiguration()
+		if errors := authzconfigvalidation.ValidateAuthorizationConfiguration(nil, authzConfiguration,
+			sets.NewString(authzmodes.AuthorizationModeChoices...),
+			sets.NewString(authzmodes.RepeatableAuthorizerTypes...),
+		); len(errors) != 0 {
+			allErrors = append(allErrors, errors.ToAggregate().Errors()...)
+		}
+
 	}
 
 	modes := sets.NewString(o.Modes...)
@@ -117,17 +146,44 @@ func (o *BuiltInAuthorizationOptions) AddFlags(fs *pflag.FlagSet) {
 	fs.DurationVar(&o.WebhookCacheUnauthorizedTTL,
 		"authorization-webhook-cache-unauthorized-ttl", o.WebhookCacheUnauthorizedTTL,
 		"The duration to cache 'unauthorized' responses from the webhook authorizer.")
+
+	fs.StringVar(&o.AuthorizationConfigurationFile, "authorization-config", o.AuthorizationConfigurationFile, ""+
+		"File with Authorization Configuration to configure the authorizer chain."+
+		"Note: This feature is in Alpha since v1.28."+
+		"The StructuredAuthorizationConfig feature needs to be set to true for enabling the functionality.")
 }
 
 // ToAuthorizationConfig convert BuiltInAuthorizationOptions to authorizer.Config
-func (o *BuiltInAuthorizationOptions) ToAuthorizationConfig(versionedInformerFactory versionedinformers.SharedInformerFactory) authorizer.Config {
-	return authorizer.Config{
-		PolicyFile:               o.PolicyFile,
-		VersionedInformerFactory: versionedInformerFactory,
-		WebhookRetryBackoff:      o.WebhookRetryBackoff,
+func (o *BuiltInAuthorizationOptions) ToAuthorizationConfig(versionedInformerFactory versionedinformers.SharedInformerFactory) (authorizer.Config, error) {
+	// When the feature flag is enabled,
+	//		the authorizer is built using the file provided through
+	// 		--authorization-config and the legacy flags are disregarded, except --authorization-policy-file,
+	//		which is required to define the ABAC Mode
+	// Else,
+	//		the AuthorizationConfiguration is built using the legacy flags.
+	if utilfeature.DefaultFeatureGate.Enabled(genericfeatures.StructuredAuthorizationConfig) && o.AuthorizationConfigurationFile != "" {
+		config, err := authzconfigloader.LoadFromFile(o.AuthorizationConfigurationFile)
+		if err != nil {
+			return authorizer.Config{}, fmt.Errorf("failed to load config from file: %s", err)
+		}
 
-		AuthorizationConfiguration: o.buildAuthorizationConfiguration(),
+		return authorizer.Config{
+			PolicyFile:                 o.PolicyFile,
+			VersionedInformerFactory:   versionedInformerFactory,
+			WebhookRetryBackoff:        o.WebhookRetryBackoff,
+			AuthorizationConfiguration: config,
+		}, nil
+	} else if utilfeature.DefaultFeatureGate.Enabled(genericfeatures.StructuredAuthorizationConfig) && o.AuthorizationConfigurationFile == "" {
+		return authorizer.Config{}, fmt.Errorf("StructuredAuthorizationConfig is enabled but no configuration file is defined")
+	} else {
+		return authorizer.Config{
+			PolicyFile:                 o.PolicyFile,
+			VersionedInformerFactory:   versionedInformerFactory,
+			WebhookRetryBackoff:        o.WebhookRetryBackoff,
+			AuthorizationConfiguration: o.buildAuthorizationConfiguration(),
+		}, nil
 	}
+
 }
 
 // buildAuthorizationConfiguration converts existing flags to the AuthorizationConfiguration format
@@ -157,6 +213,5 @@ func (o *BuiltInAuthorizationOptions) buildAuthorizationConfiguration() *authzco
 			authorizers = append(authorizers, authzconfig.AuthorizerConfiguration{Type: authzconfig.AuthorizerType(mode)})
 		}
 	}
-
 	return &authzconfig.AuthorizationConfiguration{Authorizers: authorizers}
 }
